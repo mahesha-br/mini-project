80e42aa980be8a2b7c2508b1c53fad31082a30da
mahesha-br <maheshabr2004@gmail.com>
2026-08-27 13:08:36 +0530

    logo done

--- online-learing-paltfrom/app/workspace/_components/AppSidebar.jsx
+++ online-learing-paltfrom/app/workspace/_components/AppSidebar.jsx
@@ -6,11 +6,16 @@ import {
     SidebarGroup,
     SidebarHeader,
 } from "@/components/ui/sidebar"
+import Image from "next/image";
 
 function AppSidebar() {
     return (
         <Sidebar>
-            <SidebarHeader >
+            <SidebarHeader>
+                <div className="flex items-center gap-2 p-2">
+                    <Image src={'/ai_learning_logo_only.svg'} alt="logo" width={40} height={40} />
+                    <span className="text-xl font-bold">Online Learning</span>
+                </div>
             </SidebarHeader>
             <SidebarContent>
                 <SidebarGroup />
